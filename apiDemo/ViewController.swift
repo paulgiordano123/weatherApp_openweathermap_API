33c9e0c9ea5c13aafe37b0f5cde9140337864c74
Paul Giordano <giordap3@tcnj.edu>
2019-12-26 22:02:56 -0500

    First commit

--- apiDemo/ViewController.swift
+++ apiDemo/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBAction func submit(_ sender: AnyObject) {
+        
+        // Do any additional setup after loading the view.
+        
+        print(cityTextField.text)
+        
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=" + cityTextField.text! + "&appid=1ff07de74f2a657d3bf1d135f2d47ef8") {
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            
+            if error != nil {
+                
+                print(error)
+                
+            } else {
+        
+                if let urlContent = data {
+                    
+                    do {
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                                    
+                        print(jsonResult)
+                        print(jsonResult["name"])
+                        
+                        if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
+                            
+                            DispatchQueue.main.sync {
+                                self.resultLabel.text = description
+                                
+                            }
+                        }
+                        
+                    } catch {
+                        
+                        print("there was an error")
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        task.resume()
+        
+        } else {
+            
+            resultLabel.text = "Could not find the weather for the city entered"
+        }
+        
+    }
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
